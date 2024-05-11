a318b567777cca160b0a8ad6d26fcd709a3884ae
fuyjcnbr <paall@rambler.ru>
2024-05-11 15:49:05 +0300

    model added

---
+++ model.py
@@ -0,0 +1,8 @@
+
+import tensorflow as tf
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class_names = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
